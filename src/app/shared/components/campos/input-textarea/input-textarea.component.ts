c7f38ce6d0babbdfbec95e394bc46a60c338e3cc
DaniloSoares0 <daniloxviii@gmail.com>
2021-08-16 13:58:01 -0300

    componentizando os inputs

--- src/app/shared/components/campos/input-textarea/input-textarea.component.ts
+++ src/app/shared/components/campos/input-textarea/input-textarea.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { ValidarCamposService } from '../validar-campos.service';
 
 @Component({
-  selector: 'input-textarea',
+  selector: 'dio-input-textarea',
   templateUrl: './input-textarea.component.html',
   styleUrls: ['./input-textarea.component.scss']
 })
 export class InputTextareaComponent implements OnInit {
 
-  constructor() { }
+  @Input() descricao: string;
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+
+  constructor(public validacao: ValidarCamposService) { }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName]
+  }
+
 
   ngOnInit() {
   }
+
 
 }
